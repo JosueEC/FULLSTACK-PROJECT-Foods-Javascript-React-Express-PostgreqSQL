0b54111eb83fd7f3fd4c54b9443ed008551d13ea
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-04-14 12:16:22 -0600

    feat(controllers): :sparkles: La API pued obtener recetas por coincidencias en NAME, tanto de la API externa como de la BD
    La API ya es capaz de obtener varias recetas en base a coincidencias encontradas con el name que es pasado por query en el endpoint, estas coincidencias las busca tanto en la API externa como en nuestra Base de Datos

--- api/src/controllers/recipe-controllers/services/read-recipes-query.js
+++ api/src/controllers/recipe-controllers/services/read-recipes-query.js
@@ -1,10 +1,21 @@
 require('dotenv').config()
+const { Op } = require('sequelize')
 // const { API_KEY } = process.env
+
+const { Recipe } = require('../../../db')
 
 const { URL_BASE } = require('../../../utilities/paths')
 const fetch = require('node-fetch')
 
 const readRecipesQuery = async (name) => {
+  const recipesFromDatabase = await getRecipesFromDatabase(name)
+  const recipesFromAPI = await getRecipesFromAPI(name)
+  const allRecipes = recipesFromDatabase.concat(recipesFromAPI.results)
+
+  return allRecipes
+}
+
+async function getRecipesFromAPI (name) {
   // const recipes = await fetch(`${URL_BASE}/complexSearch?${API_KEY}&query=${name}&number=100`)
   const recipes = await fetch(`${URL_BASE}/complexSearch?query=${name}`)
     .then((response) => response.json())
@@ -12,6 +23,19 @@ const readRecipesQuery = async (name) => {
       return data
     })
 
+  return recipes
+}
+
+async function getRecipesFromDatabase (name) {
+  const query = name.toUpperCase()
+  const recipes = Recipe.findAll({
+    where: {
+      title: {
+        [Op.iLike]: `%${query}%`
+      }
+    },
+    attributes: ['id', 'title', 'image']
+  })
   return recipes
 }
 
