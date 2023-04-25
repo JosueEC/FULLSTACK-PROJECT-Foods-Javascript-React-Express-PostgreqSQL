f5489740c50188bcf1f8158b51effa7c9e63613d
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-04-25 11:46:26 -0600

    style(components): :lipstick: Modificamos la etiqueta a por button en nuestro componente Button
    React nos mostraba un error en el componente Button, esto era porque usabamos una etiqueta a para este componente y no nos permite usarla en conjunto con una etiqueta Link, por lo cual se cambio por una etiqueta button para solucionar el problema

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -4,6 +4,6 @@ import styles from './Button.module.css'
 
 export default function Button ({ textButton }) {
   return (
-    <a href='#inicio' className={styles.btn}>{ textButton }</a>
+    <button className={styles.btn}>{ textButton }</button>
   )
 }
