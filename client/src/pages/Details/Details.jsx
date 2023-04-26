b895d038d8da81d25a603b7d8d0b9c5458fc2a76
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-04-26 09:52:23 -0600

    style(pages): :lipstick: Mejoramos los estilos de la Page Details
    Cambiamos los colores para que sean mas acorde a todos los que estamos usando, modificamos el tamaño de fuente y el color de la fuente y mejoramos los titulos

--- client/src/pages/Details/Details.jsx
+++ client/src/pages/Details/Details.jsx
@@ -30,12 +30,12 @@ export default function Details () {
                   <img src={recipe.image} alt='recipe'/>
                 </div>
                 <div className={styles.containerSummary}>
-                  <span>Summary</span>
+                  <span className={styles.heading}>Summary</span>
                   <p dangerouslySetInnerHTML={{__html: recipe.summary}} />
                 </div>
               </div>
               <div className={styles.basicInfo}>
-                <span>{recipe.title}</span>
+                <span className={styles.heading}>{recipe.title}</span>
               </div>
               <div className={styles.description}>
                 <ul className={styles.features}>
@@ -43,11 +43,10 @@ export default function Details () {
                   <li><i className='fa-solid fa-circle-check'></i>Diets: {recipe.diets.join(', ')}</li>
                   <li><i className='fa-solid fa-circle-check'></i>Preparation Minutes: {recipe.preparationMinutes}</li>
                   <li><i className='fa-solid fa-circle-check'></i>Servings: {recipe.servings}</li>
-                  <li><i className='fa-solid fa-circle-check'></i>Credits Text: {recipe.creditsText}</li>
+                  <li><i className='fa-solid fa-circle-check'></i>Credits: {recipe.creditsText}</li>
                 </ul>
+                <span className={styles.heading}>Instructions:</span>
                 <p dangerouslySetInnerHTML={{__html: recipe.instructions}} />
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus temporibus corporis repudiandae, consectetur nostrum nisi commodi placeat rerum molestias numquam nihil accusantium deleniti! Enim, nesciunt a quis amet hic officia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nemo accusantium tempora facere doloremque cum iusto, ut neque, fuga omnis libero laborum ullam. At dolorum qui atque labore illo dignissimos.</p>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus temporibus corporis repudiandae, consectetur nostrum nisi commodi placeat rerum molestias numquam nihil accusantium deleniti! Enim, nesciunt a quis amet hic officia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nemo accusantium tempora facere doloremque cum iusto, ut neque, fuga omnis libero laborum ullam. At dolorum qui atque labore illo dignissimos.</p>
               </div>
             </div>
           ) : (
